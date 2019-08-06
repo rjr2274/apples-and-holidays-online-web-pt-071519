98378c7ade1bccdb555acc780fbc5df6bd3da5db
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-06 00:33:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
       puts "#{season.capitalize}:"
       holidays.each do |day, stuff|
-        binding.pry
         puts "#{day.to_s.split("_").map{|word| word.capitalize}.join(" ")}: #{stuff.join(", ")}"
 
       end
